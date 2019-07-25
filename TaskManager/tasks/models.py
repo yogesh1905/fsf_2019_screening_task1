1ad0b26ca0d0ce0588010242b5a855484910be47
Yogesh <yogeshchoubey7@gmail.com>
2019-07-26 02:08:14 +0530

    Revamped the database

--- TaskManager/tasks/models.py
+++ TaskManager/tasks/models.py
@@ -1,26 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 STATUS_CHOICES = (('Planned', 'Planned'), ('Inprogress', 'Inprogress'), ('Done', 'Done'))  
 
-# below model creates a many to many relationship between user and team (or group) 
-class JoinTable(models.Model):
-	person_id = models.IntegerField()
-	group_id = models.IntegerField()
-
-
 # below model defines a group containing username of the group creator
 class Group(models.Model):
-	creator = models.CharField(max_length=100)
+	creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+# below model creates a many to many relationship between user and team (or group) 
+class JoinTable(models.Model):
+	person_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 
 # below model defines property of a task
 class Task(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=400)
-	assignee = models.CharField(max_length=100)		# Creator of the task
+	assignee = models.ForeignKey(User, on_delete=models.CASCADE)		# Creator of the task
 	status = models.CharField(max_length=100, choices=STATUS_CHOICES)	# There are 3 choices for status
-	group_id = models.IntegerField(default=-1)   # Set to -1 by default when a task doesn't belong to any group
+	group_id = models.ForeignKey(Group, on_delete=models.CASCADE, default=None, null=True)   # Set to -1 by default when a task doesn't belong to any group
 	def __str__(self):
 		return self.title
 
@@ -30,12 +30,12 @@ class Task(models.Model):
 
 # below model defines the property of a comment
 class Comment(models.Model):
-	task_id = models.IntegerField()		# The id of the task to which the comment belongs to
+	task_id = models.ForeignKey(Task, on_delete=models.CASCADE)		# The id of the task to which the comment belongs to
 	comment = models.CharField(max_length=300)
-	commenter = models.CharField(max_length=100, default="")
+	commenter = models.ForeignKey(User, on_delete=models.CASCADE)
 	
 
 # below model defines the users assigned to a given task
 class AssignTask(models.Model):
-	task_id = models.IntegerField()		
-	person_id = models.IntegerField()
+	task_id = models.ForeignKey(Task, on_delete=models.CASCADE)		
+	person_id = models.ForeignKey(User, on_delete=models.CASCADE)
--- TaskManager/tasks/views.py
+++ TaskManager/tasks/views.py
@@ -12,13 +12,13 @@ from . import forms
 # Handles the request for home page of the user
 @login_required(login_url="/accounts/login")
 def team_list(req):
-	tasks = Task.objects.filter(group_id=-1, assignee=req.user.username)
+	tasks = Task.objects.filter(group_id=None, assignee=req.user)
 
 	# Quering for the list of groups the current user belongs to
-	usergroups = JoinTable.objects.filter(person_id=req.user.id)
+	usergroups = JoinTable.objects.filter(person_id=req.user)
 	group_id_list = []
 	for group in usergroups:
-		group_id_list.append(group.group_id)
+		group_id_list.append(group.group_id.id)
 	usergroups = Group.objects.filter(id__in=group_id_list)
 	
 	return render(req, 'tasks/teamlist.html', {'usergroups': usergroups, 'tasks':tasks})
@@ -30,9 +30,9 @@ def team_list(req):
 def create_team(req):
 	if req.method == 'POST':
 		# Creates the group and saves it in the database
-		newGroup = Group(creator=req.user.username)
+		newGroup = Group(creator=req.user)
 		newGroup.save()
-		newEntry = JoinTable(person_id=req.user.id, group_id=newGroup.id)
+		newEntry = JoinTable(person_id=req.user, group_id=newGroup)
 		newEntry.save()
 		print('/tasks/' + str(newGroup.id) + '/')
 		return render(req, 'tasks/memberlist.html', {'members': [req.user] ,'creator': req.user.username, 'groupid': newGroup.id, 'curruser': req.user.username, 'newTeam': 'yes'})
@@ -44,19 +44,21 @@ def create_team(req):
 @login_required(login_url="/accounts/login")
 def member_list(req, gid):
 	# Checking if the current user belongs to the group with group_id = gid
-	if JoinTable.objects.filter(group_id=gid, person_id=req.user.id):
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
+	if JoinTable.objects.filter(group_id=group, person_id=req.user):
 		# Quering for the list of users belonging to the current group
-		memberIds = JoinTable.objects.filter(group_id=gid)
-		group = Group.objects.filter(id=gid)
-		if group:
-			group = group[0]
+		memberIds = JoinTable.objects.filter(group_id=group)
 		if memberIds:
 			members = []
 			for memberId in memberIds:
-				members.append(User.objects.filter(id=memberId.person_id)[0].username)
+				members.append(User.objects.filter(id=memberId.person_id.id)[0].username)
 			# Quering for the list of task belonging to the current group
-			tasks = Task.objects.filter(group_id=gid)
-			return render(req, 'tasks/memberlist.html', {'members': members, 'creator': group.creator, 'groupid': gid, 'curruser':req.user.username, 'tasks': tasks})
+			tasks = Task.objects.filter(group_id=group)
+			return render(req, 'tasks/memberlist.html', {'members': members, 'creator': group.creator, 'groupid': group.id, 'curruser':req.user.username, 'tasks': tasks})
 		else:
 			return render(req, 'tasks/404.html')
 	else:
@@ -67,18 +69,23 @@ def member_list(req, gid):
 # Handles the request for the page containing the list of possible users that can be added to the current team 
 @login_required(login_url="/accounts/login")
 def add_user(req, gid):
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
 	if req.method == 'GET':
 		# Checking if the current user belongs to the group with group_id = gid
-		currmem = JoinTable.objects.filter(group_id=gid)
+		currmem = JoinTable.objects.filter(group_id=group)
 		if currmem:
 			# Quering for the list of possible users that can be added
-			searchedgrp = Group.objects.filter(id=gid)[0]
-			if req.user.username == searchedgrp.creator:
+			searchedgrp = group
+			if req.user == searchedgrp.creator:
 				arr = []
 				for i in currmem:
-					arr.append(i.person_id)
+					arr.append(i.person_id.id)
 				nousers = User.objects.exclude(id__in=arr)
-				return render(req, 'tasks/adduser.html', {'nousers': nousers, 'groupid': gid})
+				return render(req, 'tasks/adduser.html', {'nousers': nousers, 'groupid': group.id})
 			else:
 				return render(req, 'tasks/404.html')
 		else:
@@ -89,9 +96,14 @@ def add_user(req, gid):
 # Handles the post request when a user tries to add another user to his team	
 @login_required(login_url="/accounts/login")
 def added(req, gid, pid):
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
 	if req.method == 'POST':
 		# Saving the user to the database
-		newEntry = JoinTable(person_id=pid, group_id=gid)
+		newEntry = JoinTable(person_id=User.objects.get(id=pid), group_id=group)
 		newEntry.save()
 		return redirect('/tasks/' + str(gid) + '/')
 
@@ -106,12 +118,12 @@ def add_individual_task(req):
 		if form.is_valid():
 			# save article to db
 			instance = form.save(commit=False)
-			instance.assignee = req.user.username
+			instance.assignee = req.user
 			instance.save()
 			return redirect('tasks:teams')
 	else:
 		form = forms.CreateTask()
-	return render(req, 'tasks/createtask.html', {'form': form})
+	return render(req, 'tasks/createtask.html', {'form': form})	
 
 
 
@@ -120,11 +132,11 @@ def add_individual_task(req):
 @login_required(login_url="/accounts/login")
 def task_detail(req, tid):
 	task = Task.objects.filter(id=tid)
-	if task and task[0].assignee == req.user.username:
+	if task and task[0].assignee == req.user:
 		task = task[0]
 		if req.method == 'GET':
 			curr_user = req.user.username
-			all_comments = Comment.objects.filter(task_id=tid).order_by('-id')
+			all_comments = Comment.objects.filter(task_id=task).order_by('-id')
 			return render(req, 'tasks/taskdetail.html', {'task': task, 'curr_user': curr_user, 'all_comments': all_comments})
 		else:
 			newComment = Comment(task_id=tid, comment=req.POST.get('comment'), commenter=req.user.username)
@@ -144,7 +156,7 @@ def edit_task(req, tid):
 	task = Task.objects.filter(id=tid)
 	if task:
 		task = task[0]
-		if task.assignee == req.user.username:
+		if task.assignee == req.user:
 			if req.method == 'POST':
 				form = forms.CreateTask(req.POST)
 				if form.is_valid():
@@ -168,7 +180,12 @@ def edit_task(req, tid):
 # Handles the request for a member belonging to a team and wanting to add a task in the team
 @login_required(login_url="/accounts/login")
 def add_group_task(req, gid):
-	curruser = JoinTable.objects.filter(group_id=gid, person_id=req.user.id)
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
+	curruser = JoinTable.objects.filter(group_id=group, person_id=req.user)
 	# Checking if the current user belongs to the team
 	if curruser:
 		if req.method == 'POST':
@@ -176,8 +193,8 @@ def add_group_task(req, gid):
 			if form.is_valid():
 				# saving article to db
 				instance = form.save(commit=False)
-				instance.assignee = req.user.username
-				instance.group_id = gid
+				instance.assignee = req.user
+				instance.group_id = group
 				instance.save()
 				return redirect('/tasks/' + str(gid) + '/')
 		else:
@@ -193,22 +210,28 @@ def add_group_task(req, gid):
 # Handles the request for page containing the details of a task belonging to a particular team
 @login_required(login_url="/accounts/login")
 def group_task_detail(req, gid, tid):
-	task = Task.objects.filter(id=tid, group_id=gid)
-	curr = JoinTable.objects.filter(group_id=gid, person_id=req.user.id)
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
+	task = Task.objects.filter(id=tid, group_id=group)
+	curr = JoinTable.objects.filter(group_id=group, person_id=req.user)
 	# Checking if the task and group exist and the task belongs to that group
 	if task and curr:
+		task = task[0]
+		curr = curr[0]
 		if req.method == 'GET':
-			assigned_users = AssignTask.objects.filter(task_id=tid)
+			assigned_users = AssignTask.objects.filter(task_id=task)
 			id_list = []
 			for user in assigned_users:
-				id_list.append(user.person_id)
+				id_list.append(user.person_id.id)
 			assigned_users = User.objects.filter(id__in=id_list)
-			task = task[0]
 			curr_user = req.user
-			all_comments = Comment.objects.filter(task_id=tid).order_by('-id')
+			all_comments = Comment.objects.filter(task_id=task).order_by('-id')
 			return render(req, 'tasks/group_task_detail.html', {'task': task, 'curr_user': curr_user, 'group_id':gid, 'task_id': tid, 'assigned_users': assigned_users, 'all_comments': all_comments})
 		else:
-			newComment = Comment(task_id=tid, comment=req.POST.get('comment'), commenter=req.user.username)
+			newComment = Comment(task_id=task, comment=req.POST.get('comment'), commenter=req.user)
 			newComment.save()
 			return redirect('/tasks/' + str(gid) + '/view-task/' + str(tid) + '/')
 	else:
@@ -220,11 +243,16 @@ def group_task_detail(req, gid, tid):
 # Handles the request for a member belong to a team and wanting to edit a team task
 @login_required(login_url="/accounts/login")
 def group_edit_task(req, gid, tid):
-	task = Task.objects.filter(id=tid, group_id=gid)
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
+	task = Task.objects.filter(id=tid, group_id=group)
 	# Checking if the task exists
 	if task:
 		task = task[0]
-		if task.assignee == req.user.username:
+		if task.assignee == req.user:
 			if req.method == 'POST':
 				form = forms.CreateTask(req.POST)
 				if form.is_valid():
@@ -249,23 +277,25 @@ def group_edit_task(req, gid, tid):
 # Handles the get request of a task creator wanting to assign a task to the members of the team
 @login_required(login_url="/accounts/login")
 def group_assign_task(req, gid, tid):
-	task = Task.objects.filter(id=tid, group_id=gid)
+	group = Group.objects.filter(id=gid)
+	if(group):
+		group = group[0]
+	else:
+		group = None
+	task = Task.objects.filter(id=tid, group_id=group)
 	if task:
 		task = task[0]
 		# Checking if the current user is the creator of the task
-		if task.assignee == req.user.username:
-			already_assigned = AssignTask.objects.filter(task_id=tid)
+		if task.assignee == req.user:
+			already_assigned = AssignTask.objects.filter(task_id=task)
 			assigned_id = []
 			for person in already_assigned:
-				assigned_id.append(person.person_id)	
-			not_assigned = User.objects.exclude(id__in=assigned_id)
+				assigned_id.append(person.person_id.id)	
+			not_assigned_list = User.objects.exclude(id__in=assigned_id)
+			not_assigned = JoinTable.objects.filter(person_id__in=not_assigned_list, group_id=group)
 			temp_id = []
 			for user in not_assigned:
-				temp_id.append(user.id)
-			not_assigned = JoinTable.objects.filter(person_id__in=temp_id, group_id=gid)
-			temp_id = []
-			for user in not_assigned:
-				temp_id.append(user.person_id)
+				temp_id.append(user.person_id.id)
 			not_assigned = User.objects.filter(id__in=temp_id)
 			return render(req, 'tasks/assign_user.html', {'not_assigned': not_assigned, 'group_id': gid, 'task_id': tid})
 		else:
@@ -279,7 +309,7 @@ def group_assign_task(req, gid, tid):
 # Handles the post request of a task creator wanting to assign a task to the members of the team 
 @login_required(login_url="/accounts/login")
 def group_task_assigned(req, gid, tid, pid):
-	assigned_user = AssignTask(task_id=tid, person_id=pid)
+	assigned_user = AssignTask(task_id=Task.objects.get(id=tid), person_id=User.objects.get(id=pid))
 	assigned_user.save()
 	return redirect('/tasks/' + str(gid) + '/view-task/' + str(tid) + '/')
 
